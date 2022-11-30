6b44b3147e8603c7ed367932dcecd0a1cfb7393d
Mike Bostock <michael.bostock@opencastsoftware.com>
2022-11-30 23:25:14 +0000

    Initial commit

---
+++ src/main/kotlin/mpbostock/FileIO.kt
@@ -0,0 +1,7 @@
+package mpbostock
+
+object FileIO {
+    fun <T> readInput(filename: String, lineMapper: (String) -> T): List<T> {
+        return this::class.java.getResourceAsStream(filename).bufferedReader().readLines().map(lineMapper)
+    }
+}
